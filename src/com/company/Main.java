42e76b737c8ac5f125ff169637ae9f0fb67559c6
Emil Madsen <70597783+MizzleTheHorse@users.noreply.github.com>
2022-02-28 10:38:24 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	system.out.println("Phillip lugter")
+	for (i=0; i<100; i++) {
+		system.out.println("Phillip lugter")
+		}
+	
     }
 }
