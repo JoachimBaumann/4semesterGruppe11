c2b5417138f49222cb146e3231daec383a1c8cfc
Joachim Baumann <jobau19@student.sdu.dk>
2022-03-28 11:37:20 +0200

    added map component

---
+++ Zombie/OSGiMap/src/main/java/dk/sdu/mmmi/cbse/osgimap/MapImpl.java
@@ -0,0 +1,27 @@
+package dk.sdu.mmmi.cbse.osgimap;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import dk.sdu.mmmi.cbse.common.data.WorldMap;
+
+public class MapImpl extends WorldMap {
+
+    public MapImpl() {
+    }
+
+    @Override
+    public void create() {
+        TiledMap map = new TiledMap();
+
+        TmxMapLoader loader = new TmxMapLoader();
+        map = loader.load("maps/mapforproject.tmx");
+        this.setMap(map);
+
+        this.setRenderer(new OrthogonalTiledMapRenderer(map));
+        this.setSr(new ShapeRenderer());
+        Gdx.gl.glLineWidth(3);
+    }
+}
---
+++ Zombie/OSGiMap/src/main/java/dk/sdu/mmmi/cbse/osgimap/MapPlugin.java
@@ -0,0 +1,17 @@
+package dk.sdu.mmmi.cbse.osgimap;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class MapPlugin implements IGamePluginService {
+    @Override
+    public void start(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+
+    }
+}
