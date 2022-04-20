839f3ac5f5727a062ed8b74f51dbb81cef1607f7
Nadia <nadianiemier@hotmail.com>
2022-04-20 12:54:10 +0200

    Asset working

--- Zombie/OSGiCommonPlayer/src/main/java/dk/sdu/mmmi/cbse/common/player/Player.java
+++ Zombie/OSGiCommonPlayer/src/main/java/dk/sdu/mmmi/cbse/common/player/Player.java
@@ -2,15 +2,15 @@ package dk.sdu.mmmi.cbse.common.player;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import dk.sdu.mmmi.cbse.common.data.AssetLoader;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
 public class Player extends Entity {
 
 
-
     @Override
     public void create() {
-        this.setSprite(new Sprite(new Texture("C:\\Users\\kinky\\IdeaProjects\\4semesterGruppe11\\Zombie\\OSGiCommon\\src\\main\\java\\dk\\sdu\\mmmi\\cbse\\common\\Assets\\png.jpg"),32,64));
+        this.setSprite(new Sprite(new Texture(AssetLoader.getAssetPath("player.png")),32,64));
 
 
     }
