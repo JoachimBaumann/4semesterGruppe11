75802ee043cecbae661e52356c1451b84281f796
MizzleTheHorse <emil.madsen@live.dk>
2022-04-27 14:46:20 +0200

    din mor

--- Zombie/OSGiEnemyTracking/src/main/java/dk/sdu/mmmi/cbse/osgienemytracking/EnemyTrackingSystem.java
+++ Zombie/OSGiEnemyTracking/src/main/java/dk/sdu/mmmi/cbse/osgienemytracking/EnemyTrackingSystem.java
@@ -22,31 +22,37 @@ public class EnemyTrackingSystem implements IPostEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
-        //get current player position
-        Entity player = world.getEntities(Player.class).get(0); //This works because there is only 1 player object.
-        PositionPart playerPositionPart = player.getPart(PositionPart.class);
+        try {
+            //get current player position
+            Entity player = world.getEntities(Player.class).get(0); //This works because there is only 1 player object.
+            PositionPart playerPositionPart = player.getPart(PositionPart.class);
 
 
-        //get all enemy positions
-        for (Entity entity : world.getEntities(Enemy.class)) {
+            //get all enemy positions
+            for (Entity entity : world.getEntities(Enemy.class)) {
 
-            //Get enemy data positions
-            PositionPart enemyPositionPart = entity.getPart(PositionPart.class);
-            EnemyMovingPart enemyMovingPart = entity.getPart(EnemyMovingPart.class);
+                //Get enemy data positions
+                PositionPart enemyPositionPart = entity.getPart(PositionPart.class);
+                EnemyMovingPart enemyMovingPart = entity.getPart(EnemyMovingPart.class);
 
 
-            if (playerPositionPart.getX() > enemyPositionPart.getX()){
-                enemyMovingPart.setRight(true);
-                enemyMovingPart.setSpace(false);
-                enemyMovingPart.setLeft(false);
-            } else {
-                enemyMovingPart.setLeft(true);
-                enemyMovingPart.setSpace(false);
-                enemyMovingPart.setRight(false);
+                if (playerPositionPart.getX() > enemyPositionPart.getX()) {
+                    enemyMovingPart.setRight(true);
+                    enemyMovingPart.setSpace(false);
+                    enemyMovingPart.setLeft(false);
+                } else {
+                    enemyMovingPart.setLeft(true);
+                    enemyMovingPart.setSpace(false);
+                    enemyMovingPart.setRight(false);
+                }
+
+                //Check if player is above and if enemy is stuck in ground position
+                if (playerPositionPart.getY() > enemyPositionPart.getY() && enemyPositionPart.isStuck())
+                    enemyMovingPart.setSpace(true);
             }
 
-            //Check if player is above and if enemy is stuck in ground position
-            if (playerPositionPart.getY() > enemyPositionPart.getY() && enemyPositionPart.isStuck()) enemyMovingPart.setSpace(true);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Exception in Enemy Tracking System: " + e.toString());
         }
     }
 }
