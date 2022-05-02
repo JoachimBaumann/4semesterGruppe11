42b8abf546ba19876c6e5e6fb0fa04e6a79a4837
Emil Madsen <70597783+MizzleTheHorse@users.noreply.github.com>
2022-05-02 12:23:15 +0200

    din mor

---
+++ Zombie/OSGiCommon/src/main/resources/Assets/hyptosis_tile-art-batch-2.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="hyptosis_tile-art-batch-1" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="hyptosis_tile-art-batch-1.png" width="960" height="960"/>
+ <tile id="815">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="816">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Zombie/OSGiPlayer/src/test/java/dk/sdu/mmmi/cbse/osgiplayer/PlayerPluginTest.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.osgiplayer;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class PlayerPluginTest {
+    //Mocks are used to isolate components during tests
+
+    @Test
+    public void startTest(){
+        System.out.println("start");
+
+        GameData gameData = mock(GameData.class); //creates a mock object
+        World world = mock(World.class); //creates another mock object
+        when(world.addEntity(any(Entity.class))).thenReturn("1"); //when addEntity() is called then return "1"
+
+        PlayerPlugin instance = new PlayerPlugin(); //creates PlayerPlugin object
+        instance.start(gameData, world);
+
+        verify(world).addEntity(any(Entity.class)); //verifies that the method is called with the right parameters
+        //behavioral testing - verifies that a certain behavior happened once
+
+    }
+
+    @Test
+    public void stopTest(){
+
+    }
+}
