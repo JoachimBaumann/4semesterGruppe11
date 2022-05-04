a31b250f5a43bd72897cea11caff38bb8b1f6e5f
faezeh <rfaezeh2@gmail.com>
2022-05-04 13:52:31 +0200

    assetLoader updated

--- Zombie/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/AssetLoader.java
+++ Zombie/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/AssetLoader.java
@@ -1,16 +1,30 @@
 package dk.sdu.mmmi.cbse.common.data;
 
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class AssetLoader {
 
-    private static final String assetPath = "\\Zombie\\OSGiCommon\\src\\main\\resources\\Assets\\";
+    private static final String macPath = "/Zombie/OSGiCommon/src/main/resources/Assets/";
+    private static final String windowsPath = "\\Zombie\\OSGiCommon\\src\\main\\resources\\Assets\\";
 
     public static String getAssetPath(String fileName) {
+        String assetPath;
+
         Path currentRelativePath = Paths.get("");
+
+        if (Objects.equals(System.getProperty("os.name"), "Mac OS X")){
+            assetPath = macPath;
+        } else {
+            assetPath = windowsPath;
+        }
+
         String workingPath = currentRelativePath.toAbsolutePath().getParent().getParent().toString();
         return workingPath + assetPath + fileName;
     }
 }
+
+
 
