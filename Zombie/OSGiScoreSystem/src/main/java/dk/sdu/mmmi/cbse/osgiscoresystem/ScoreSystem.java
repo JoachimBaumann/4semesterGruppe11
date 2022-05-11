90a020a476dad2fcfb20f812c2de0389be4871fc
Emil Madsen <70597783+MizzleTheHorse@users.noreply.github.com>
2022-05-11 14:21:01 +0200

    Implemented score system to add playerscore to text file.

--- Zombie/OSGiScoreSystem/src/main/java/dk/sdu/mmmi/cbse/osgiscoresystem/ScoreSystem.java
+++ Zombie/OSGiScoreSystem/src/main/java/dk/sdu/mmmi/cbse/osgiscoresystem/ScoreSystem.java
@@ -7,13 +7,19 @@ import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.data.entityparts.*;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 
 public class ScoreSystem implements IGamePluginService {
 
+
+    private String path = AssetLoader.getAssetPath("\\scores\\scores.txt");
 
     //Different implementations of Maps offers different time complexities - https://www.geeksforgeeks.org/differences-treemap-hashmap-linkedhashmap-java/
     //TreeMap insert, delete, search offers O(log(n)) time
@@ -39,5 +45,28 @@ public class ScoreSystem implements IGamePluginService {
         pass
     }
 
+
+    /**
+     * Loads comma separated text file and adds entries to Map structure
+     */
+    private void loadScores() {
+
+        String lines;
+        try {
+            File scores = new File("filename.txt");
+            Scanner myReader = new Scanner(scores);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred reading from file: " + e.toString());
+        }
+    }
+
+    private void treeInsert(int key, int value) {
+        treeMapScores.put(key, value);
+    }
 
 }
