eb15546f199f51f4592887ad26b4d1bbb41a281d
Emil Madsen <70597783+MizzleTheHorse@users.noreply.github.com>
2022-05-18 12:56:18 +0200

    .

--- Zombie/OSGiEnemy/src/main/java/dk/sdu/mmmi/cbse/osgienemy/EnemyControlSystem.java
+++ Zombie/OSGiEnemy/src/main/java/dk/sdu/mmmi/cbse/osgienemy/EnemyControlSystem.java
@@ -26,10 +26,7 @@ public class EnemyControlSystem implements IEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
-        enemies = world.getEntities(EnemyBat.class);
-        enemies.addAll(world.getEntities(EnemyRaven.class));
-        enemies.addAll(world.getEntities(EnemySnail.class));
-        enemies.addAll(world.getEntities(EnemyZombie.class));
+        enemies = world.getEnemies();
 
         //System.out.println(enemies.size());
         for (Entity enemy : enemies) {
