4035412949e242bf93f771c5ba6b3af901fc67bf
Emil Madsen <70597783+MizzleTheHorse@users.noreply.github.com>
2022-05-26 11:48:34 +0200

    push it to the limit

--- Zombie/OSGiBoss/src/test/java/dk/sdu/mmmi/cbse/osgiboss/PathFinderTest.java
+++ Zombie/OSGiBoss/src/test/java/dk/sdu/mmmi/cbse/osgiboss/PathFinderTest.java
@@ -8,8 +8,10 @@ import dk.sdu.mmmi.cbse.osgiboss.AStar.GridCell;
 import dk.sdu.mmmi.cbse.osgiboss.AStar.NavTmxMapLoader;
 import dk.sdu.mmmi.cbse.osgiboss.AStar.NavigationTiledMapLayer;
 import dk.sdu.mmmi.cbse.osgiboss.AStar.Pathfinder;
-import org.junit.*;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 import java.util.List;
 
@@ -30,17 +32,20 @@ public class PathFinderTest {
     List<GridCell> path;
 
 
-    @Test
-    public void startTest() {
+    @Before
+    public void loadSettings() {
         map = loader.load(AssetLoader.getAssetPath(assetPath, "finalmap.tmx"));
         navLayer = (NavigationTiledMapLayer) map.getLayers().get("navigation");
         navLayer.setHeight(30);
         navLayer.setWidth(360);
         pathfinder = new Pathfinder(navLayer);
-
         System.out.println("Testing Path finding for coordinates (1500, 500)");
-
         path = pathfinder.findPath(navLayer.getCell(250, 100) , navLayer.getCell(1500, 500));
+    }
+
+
+    @Test
+    public void startTest() {
         System.out.println("Starting pathfinding test");
         System.out.println(path);
         if (path == null) {
